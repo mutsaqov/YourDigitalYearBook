b15ee4de0a9b4f9ceaf942164c0d69e7d942f8a9
mutsaqov <mutsaqovativ@gmail.com>
2021-03-10 15:53:10 +0700

    Update
    Update Splash and Animation

--- app/src/main/java/com/example/myannualbook/Splash.java
+++ app/src/main/java/com/example/myannualbook/Splash.java
@@ -4,35 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
-    private TextView tv_1, tv_2, tv_3, tv_4;
+    private TextView tv_1, tv_2, tv_3;
     private ImageView iv;
+    private ProgressBar pb;
+
+    private View satu, dua, tiga, empat, lima;
+    //animation
+    Animation topAnimation, bottomAnimation, middleAnimation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         tv_1 = (TextView) findViewById(R.id.textatas);
-        tv_2 = (TextView) findViewById(R.id.text2020);
-        tv_3 = (TextView) findViewById(R.id.by);
-        tv_4 = (TextView) findViewById(R.id.freeze);
+        tv_2 = (TextView) findViewById(R.id.textbwh);
+        tv_3 = findViewById(R.id.welcome);
 
         iv = (ImageView) findViewById(R.id.val);
+        pb = findViewById(R.id.pbar);
 
-        Animation animation = AnimationUtils.loadAnimation(this,R.anim.transition);
-        tv_1.startAnimation(animation);
-        iv.startAnimation(animation);
-        tv_2.startAnimation(animation);
-        tv_3.startAnimation(animation);
-        tv_4.startAnimation(animation);
+        satu = findViewById(R.id.firstline);
+        dua = findViewById(R.id.secondline);
+        tiga = findViewById(R.id.thirdline);
+        empat = findViewById(R.id.fourthline);
+        lima = findViewById(R.id.fifthline);
 
-        final Intent intent = new Intent(this, Register.class);
+
+        //Animations
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        middleAnimation = AnimationUtils.loadAnimation(this, R.anim.mid_anim);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+
+        //Load The Animations
+        satu.setAnimation(topAnimation);
+        dua.setAnimation(topAnimation);
+        tiga.setAnimation(topAnimation);
+        empat.setAnimation(topAnimation);
+        lima.setAnimation(topAnimation);
+
+        iv.setAnimation(middleAnimation);
+        tv_1.setAnimation(middleAnimation);
+        tv_2.setAnimation(middleAnimation);
+        tv_3.setAnimation(bottomAnimation);
+
+        pb.setAnimation(bottomAnimation);
+
+
+        final Intent intent = new Intent(this, teacher_register.class);
         Thread timer = new Thread() {
             public void run() {
              try {
--- app/src/main/java/com/example/myannualbook/teacher_register.java
+++ app/src/main/java/com/example/myannualbook/teacher_register.java
@@ -3,12 +3,19 @@ package com.example.myannualbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class teacher_register extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_register);
+
+
     }
 }
